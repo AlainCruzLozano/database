ee801df98856fbcfc9a2d15fde44fa5126b57937
Osmar Cruz Lozano <isc20350262@gmail.com>
2023-10-27 13:37:04 -0600

    fix git bug

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,9 @@
 const {request, response} = require ('express');
+const bcrypt = require('bcrypt');
 const usersModel = require('../models/users');
 const pool = require('../db');
 
-const usersList= async(req = request,res=response)=>{
+const usersList= async(req=request,res=response)=>{
     let conn;
     try {
         conn = await pool.getConnection();
@@ -25,12 +26,9 @@ const usersList= async(req = request,res=response)=>{
 
     const listUserByID = async (req=request, res= response)=>{
         const {id} = req.params;
-        
-        if (isNaN(id));
+        /*if (isNaN(id));
         res.status(404).json({msg:'Invalid ID'});
-        return
-
-
+        return;*/
 
         let conn;
         try{ //evitar errores y lo envia al catch
@@ -43,15 +41,15 @@ const usersList= async(req = request,res=response)=>{
             })
             if (!user){
                 res.status(404).json({msg:'User Not Found'});
-                
+                return;
             }
     
             res.json(user);
-            
+            return;
         } catch (error){
             console.log(error);
-            res.status(500).json({msg:"Error Connecting to MySQL database"});
-        
+            res.status(500).json({msg:'Error Connecting to MySQL database'});
+            return;
         }finally{
             conn.end();
             if (conn) conn.end();
@@ -74,8 +72,10 @@ const usersList= async(req = request,res=response)=>{
                 res.status(400).json({msg:'Missing information'});
                 return;
             }
+            const saltRounds = 10; 
+            const passwordHash = await bcrypt.hash(password, saltRounds);
 
-            const user = [user,name, email, password, name, lastname, phone_number, role_id, is_active];
+            const user = [username, email, passwordHash, name, lastname, phone_number, role_id, is_active];
 
             let conn;
 
@@ -89,7 +89,7 @@ const usernameuser  = await conn.query(
         if (err) throw err;}
 );
     if (usernameuser){
-        res.status(409).json({msg: `user with name ${username} already exist`})
+        res.status(409).json({msg: 'user with name ${username} already exist'})
     }
 
 
@@ -100,16 +100,16 @@ const emailUser  = await conn.query(
             if (err) throw err;}
     );
     if (email){
-        res.status(409).json({msg: `user with name ${email} already exist`})
+        res.status(409).json({msg: 'user with name ${email} already exist'})
     }
 
 
     const userAdded = await conn.query(usersModel.addRow, [...user], (err)=>{
         if (err) throw err;
     })
-    if (userAdded.affectedRow === 0) throw new Error( {msg:"Failed add user"});
+    if (userAdded.affectedRow === 0) throw new Error( {msg:'Failed add user'});
 
-    res.json({msg: "user added succesfully"});
+    res.json({msg: 'user added succesfully'});
 
 
 }catch (error){
@@ -120,4 +120,201 @@ const emailUser  = await conn.query(
 }
 }
 
-        module.exports = {usersList, listUserByID, addUser};
+/*
+const updateUser = async (req = request, res = response) => {
+    let conn;
+
+    try{
+    
+        conn = pool.getConnection();
+            const {id} = req.params;
+    
+            const [userExists] = (await conn).query(
+                userModel.getByID,
+                [id],
+                (err) => {if (err) throw err;}
+            );
+            if (!userExists || userExists.id <= 1){
+                res.status(404).json({msg: 'Username not found'});
+                return;
+            }
+            
+            const userUpdated = (await conn).query(
+                userModel.updateRow,
+                [id],
+                (err) => {if (err) throw err;}
+            );
+            if(userUpdated.affectedRow === 0){
+                throw new Error({msg: 'Failed to update user'})
+            };
+            res.json({msg:'User updated succesfully'});
+    
+    } catch(error){
+        console.log(error);
+        res.status(500).json(error);
+    } finally{
+        if (conn) conn.end();
+    }
+    }
+    */
+
+    const updateUser = async (req = request, res = response) => {
+        const{
+            username,
+            email,
+            password,
+            name,
+            lastname,
+            phone_number,
+            role_id,
+            is_active
+        } = req.body;
+    
+    const {id}= req.params;
+
+    let passwordHash;
+    if(password){
+        const saltRounds=10;
+        passwordHash = await bcrypt.hash(password, saltRounds);
+    }
+
+    let newUserData = [
+        username,
+        email,
+        passwordHash,
+        name,
+        lastname,
+        phone_number,
+        role_id,
+        is_active];
+    
+        let conn;
+    
+    try{
+        conn = pool.getConnection();
+            const {id} = req.params;
+    
+            const [userExists] = (await conn).query(
+                userModel.getByID,
+                [id],
+                (err) => {if (err) throw err;}
+            );
+            if (!userExists || userExists.is_active === 0){
+                res.status(404).json({msg: 'User not found'});
+                return;
+            }
+            if (username){
+            const [usernameuser]  = await conn.query(
+                usersModel.getBYUserame,
+                [username],
+                (err) =>{
+                    if (err) throw err;}
+            );
+                if (usernameuser){
+                    res.status(409).json({msg: `user with name ${username} already exist`})
+                    return;
+                }
+            }
+            if (email){
+                res.status(409).json({msg: `user with name ${email} already exist`});
+                return;
+            }
+        
+        const oldUserData = [
+            userExists.username,
+            userExists.email,
+            userExists.password,
+            userExists.name,
+            userExists.lastname,
+            userExists.phone_number,
+            userExists.role_id,
+            userExists.is_active
+        ];
+        newUserData.forEach((userData) => { //llena el arreglo con la base de datos del que no mandamos a llamar
+            if(!userData){
+                newUserData[index] = oldUserData[index];
+            }
+        });
+    
+        const [userUpdated] = await conn.query(
+            userModel.updateRow,
+            [...newUserData, id],
+            (err) => {if(err) throw err;}
+        )
+        if (userUpdated.affectedRow === 0){
+            throw new Error('User not updated');
+        }
+        res.json({msg:'User updated successfully'})
+    
+    } catch(error){
+        console.log(error);
+        res.status(500).json(error);
+    } finally{
+        if (conn) conn.end();
+    }
+    }
+    
+
+    const deleteUser = async (req = request, res = response) => {
+        let conn;
+        try{
+            conn = pool.getConnection();
+            const {id} = req.params;
+    
+            const [userExists] = (await conn).query(
+                userModel.getByID,
+                [id],
+                (err) => {if (err) throw err;}
+            );
+            if (!userExists || userExists.is_active === 0){
+                res.status(404).json({msg: 'User not found'});
+                return;
+            }
+            const userDeleted = (await conn).query(
+                userModel.deleteRow,
+                [id],
+                (err) => {if (err) throw err;}
+            );
+            if(userDeleted.affectedRow === 0){
+                throw new Error({msg: 'Failed to delete user'})
+            };
+            res.json({msg:'User deleted succesfully'});
+    } catch(error){
+        console.log(error);
+        res.status(500).json(error);
+    } finally{
+        if (conn) conn.end();
+    }
+    }
+    
+
+
+
+    const iaiubd = async (res=response, req=request) => {
+        let conn;
+    };
+    try{
+        conn = {
+
+        }
+        const passwordOk = bcrypt.compare(password, user.password);
+        if (!passwordOk){
+            res.status(404).json({msg:'Wrong username or password'});
+            return;
+        }
+        delete user.password;
+        delete user.created_at;
+        delete user.updated_at;
+
+        res.json(user);
+
+    } catch(error){
+        console.log(error);
+        res.status(500).json(error);
+    } finally{
+        if (conn) conn.end();
+    }
+    
+
+
+        module.exports = {usersList, listUserByID, updateRow, addUser, updateUser};
